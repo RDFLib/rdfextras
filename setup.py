3d1020a2c8fb80172225d6ab2d1eb283b24901d2
Daniel Krech <eikeon@eikeon.com>
2010-02-16 13:47:04 -0500

    initial commit of pure python sparql bits from rdflib/trunk as of r1843

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 from distutils.core import setup
 
 from rdfextras import __version__
@@ -8,7 +8,7 @@ setup(
     name = 'rdfextras',
     version = __version__,
     description = "RDFExtras provide tools, extra stores and such for RDFLib.",
-    author = "Niklas Lindström",
+    author = "Niklas LindstrÃ¶m",
     author_email = "lindstream@gmail.com",
     url = "http://code.google.com/p/rdfextras/",
     license = "BSD",
@@ -18,6 +18,9 @@ setup(
                    "Topic :: Software Development :: Libraries :: Python Modules",
                    "Operating System :: OS Independent",
                    ],
-    packages = ['rdfextras'],
+    packages = ['rdfextras',
+                'rdfextras.tools',
+                'rdfextras.sparql',
+                ],
     )
 
