be70d4cd612154af95a8405506c81cdad077da98
Niklas Lindstrom <lindstream@gmail.com>
2010-02-21 00:07:11 +0100

    small setup reshuffle

--- setup.py
+++ setup.py
@@ -1,22 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-extra_args = {}
-try:
-    from setuptools import setup
-    extra_args['entry_points'] = {        
-        'console_scripts': [
-            'rdfpipe = rdfextras.tools.rdfpipe:main',
-        ],
-        'nose.plugins': [
-            'EARLPlugin = rdfextras.tools.EARLPlugin:EARLPlugin',
-        ],
-    }
-except ImportError:
-    from distutils.core import setup
-
 from rdfextras import __version__
 
-setup(
+config = dict(
     name = 'rdfextras',
     version = __version__,
     description = "RDFExtras provide tools, extra stores and such for RDFLib.",
@@ -34,5 +20,29 @@ setup(
                 'rdfextras.tools',
                 'rdfextras.sparql',
                 ],
-    **extra_args)
+)
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+else:
+    config.update(
+        entry_points = {
+            'console_scripts': [
+                'rdfpipe = rdfextras.tools.rdfpipe:main',
+            ],
+            'nose.plugins': [
+                'EARLPlugin = rdfextras.tools.EARLPlugin:EARLPlugin',
+            ]
+        },
+        #test_suite = 'nose.collector',
+        #namespace_packages = ['rdfextras'], # TODO: really needed?
+        #install_requires = [
+        #    'rdflib >= 3.0',
+        #    'pyparsing'
+        #],
+    )
+
+setup(**config)
 
