65cab3436920aa138868af9ef3a137a6d2f9a60a
Gunnar Aastrand Grimnes <gromgull@gmail.com>
2011-08-21 13:01:48 +0200

    Update issue 9. This is indeed broken - I've added a test for it, but could not see an immediate fix.

--- test/test_sparql/test_not_equals.py
+++ test/test_sparql/test_not_equals.py
@@ -29,5 +29,30 @@ def testSPARQLNotEquals():
         item = row[0]
         assert item == URIRef("http://example.org/bar"), "unexpected item of '%s'" % repr(item)
 
+
+def testSPARQLNotEqualsNegative():
+
+    """
+    This is rdfextras issue 9
+    """
+
+    NS = u"http://example.org/"
+    graph = ConjunctiveGraph()
+    graph.parse(StringInputSource("""
+       @prefix    : <http://example.org/> .
+       @prefix rdf: <%s> .
+       :foo rdf:value 1.
+       :bar rdf:value -2.""" % RDF.uri), format="n3")
+    rt = graph.query("""SELECT ?node 
+                        WHERE {
+                                ?node rdf:value ?val.
+                                FILTER (?val < -1)
+                               }""",
+                           initNs={'rdf': RDF.uri},
+                           DEBUG=False)
+    for row in rt:        
+        item = row[0]
+        assert item == URIRef("http://example.org/bar"), "unexpected item of '%s'" % repr(item)
+
 if __name__ == '__main__':
     testSPARQLNotEquals()
