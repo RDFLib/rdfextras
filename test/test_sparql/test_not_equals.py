72af2dc1698abd08896d28b7c41c76c04a482f75
Graham Higgins <gjh@bel-epa.com>
2011-09-20 16:21:32 +0100

    Marked failing tests as 'known_issue', added corresponding issues to tracker.

--- test/test_sparql/test_not_equals.py
+++ test/test_sparql/test_not_equals.py
@@ -54,5 +54,21 @@ def testSPARQLNotEqualsNegative():
         item = row[0]
         assert item == URIRef("http://example.org/bar"), "unexpected item of '%s'" % repr(item)
 
+testSPARQLNotEqualsNegative.known_issue = True
+"""
+ERROR: test_describe (test_describe.TestDescribe)
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "test/test_sparql/test_describe.py", line 19, in test_describe
+    res=g.query("DESCRIBE <urn:a>")
+  File "rdflib/graph.py", line 804, in query
+    return result(processor.query(query_object, initBindings, initNs, **kwargs))
+  File "rdfextras/sparql/processor.py", line 45, in query
+    extensionFunctions=extensionFunctions)
+  File "rdfextras/sparql/algebra.py", line 322, in TopEvaluate
+    expr = reduce(ReduceToAlgebra,query.query.whereClause.parsedGraphPattern.graphPatterns,
+  AttributeError: 'NoneType' object has no attribute 'parsedGraphPattern'
+"""
+
 if __name__ == '__main__':
     testSPARQLNotEquals()
