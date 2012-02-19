f5fdbbef03a02d9de0b081da79334871f06dd936
Graham Higgins <gjh-github@bel-epa.com>
2012-02-19 07:59:30 +0000

    Skip long, expensive tests.

--- test/test_spb2_sparql.py
+++ test/test_spb2_sparql.py
@@ -1,12 +1,22 @@
 import os
+import sys
+import platform
+from nose.exc import SkipTest
+if platform.system() == 'Java':
+    raise SkipTest("Skipping, too taxing for Jython")
+import unittest
 import logging
 import time
-import unittest
 from glob import glob
 import rdflib
 from rdflib.graph import Graph
+try:
+    maketrans = str.maketrans
+except AttributeError:
+    from string import maketrans
 
 
+# 
 log = logging.getLogger(__name__)
 DEBUG = False
 EVALUATE = True
@@ -24,35 +34,82 @@ global g
 fn = os.path.dirname(os.path.abspath(__file__))+'/sp2b/10kdata.n3'
 g = create_graph(fn)
 
+skiptests = [
+    'q01',
+    'q02',
+    'q03a',
+    'q03b',
+    'q03c',
+    'q04',
+    'q05a',
+    'q05b',
+    'q06',
+    'q07',
+    'q08',
+    'q09',
+    'q10',
+    'q11',
+    'q12a',
+    'q12b',
+    'q12c',
+]
+class Envelope(object):
+    def __init__(self, n, f):
+        self.name = n
+        self.file = f
+    def __repr__(self):
+        return self.name
 
-class TestSimpleQueries(unittest.TestCase):
-    def test_sbp2(self):
-        import sys
-        if sys.platform.startswith("linux2"):
-            return # Interminable, almost literally
+def generictest(e):
+    """Documentation"""
+    if e.skip:
+        raise SkipTest("long SPARQL test %s skipped" % e.name)
+    q = open(e.file).read()
+    t1 = time.time()
+    result = g.query(q, processor='sparql')
+    # assert result == "not likely", result
+    t2 = time.time()
+    if getattr(result, 'result', False):
+        if not result.result:
+            def stab(result):
+                if result.askAnswer[0]:
+                    return [True]
+                else:
+                    []
+            if result.askAnswer:
+                res = stab(result)
+            else:
+                res = []
         else:
-            print("Graph loaded.")
-            here_dir = os.path.dirname(os.path.abspath(__file__))
-            if not 'sp2b' in here_dir:
-                os.chdir(here_dir+'/sp2b')
-            for idx, testFile in enumerate(glob('queries/*.sparql')): #[40:50]):
-                if idx in [5,6]:
-                    continue
-                q = open(testFile).read()
-                t1 = time.time()
-                result = g.query(q,processor='sparql')
-                t2 = time.time()
-                if not result.result:
-                    def stab(result):
-                        if result.askAnswer[0]:
-                            return [True]
-                        else:
-                            []
-                    if result.askAnswer:
-                        result.result = stab(result)
-                    else:
-                        result.result = []
-                print("Q%s\t%s\t%fs" % (testFile[9:-7], len(result.result), t2-t1))
+            res = result.result
+    else:
+        res = []
+    print("Q%s\t%s\t%fs" % (e.file[9:-7], len(res), t2-t1))
 
-if __name__ == '__main__':
-    unittest.main()
+
+def test_cases():
+    from copy import deepcopy
+    here_dir = os.path.dirname(os.path.abspath(__file__))
+    if not 'sp2b' in here_dir:
+        os.chdir(here_dir+'/sp2b')
+    for idx, testFile in enumerate(glob('queries/*.sparql')): #[40:50]):
+        if idx in [5,6]:
+            continue
+        gname = testFile.split('queries/')[1][:-7].translate(maketrans('-/','__'))
+        e = Envelope(gname, testFile)
+        if gname in skiptests:
+            e.skip = True 
+        else:
+            e.skip = False
+        # e.skip = True
+        if sys.version_info[:2] == (2,4):
+            import pickle
+            gjt = pickle.dumps(generictest)
+            gt = pickle.loads(gjt)
+        else:
+            gt = deepcopy(generictest)
+        gt.__doc__ = testFile
+        yield gt, e
+
+if __name__ == "__main__":
+    test_cases()
