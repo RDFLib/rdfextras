8dd52a9617a1b315e80fc2b12d8fb9cc7297b171
Graham Higgins <gjh-github@bel-epa.com>
2012-07-12 16:38:27 +0100

    Remove inessential imports, add text note referencing Sesame2 2.6.6 issue.

--- test/test_store/test_sparql.py
+++ test/test_store/test_sparql.py
@@ -6,20 +6,49 @@ except ImportError:
 
 import logging
 
+# from test_context import ContextTestCase
+# from test_graph import GraphTestCase
+# from rdflib import BNode, Literal, RDF, URIRef
+# import rdflib.term
+from rdflib import Graph
+
 _logger = logging.getLogger(__name__)
-
-from test_context import ContextTestCase
-from test_graph import GraphTestCase
-from rdflib import URIRef, BNode, Literal, RDF, Graph
-import rdflib.term
 storetest = True
-
 graph = Graph(store="SPARQL")
+
 
 def setUp(self):
     graph.open("http://lod.openlinksw.com/sparql/", create=False)
     ns = list(graph.namespaces())
     assert len(ns) > 0, ns
 
+
 def tearDown(self):
     self.graph.close()
+
+"""
+Reported problem with Sesame2 2.6.6:
+
+POST /openrdf-sesame/repositories/test/statements HTTP/1.1
+Accept-Encoding: identity
+Content-Length: 226
+Host: localhost:8080
+Accept: application/sparql-results+xml
+User-Agent: sparqlwrapper 1.5.0 (http://sparql-wrapper.sourceforge.net/)
+Connection: close
+Content-Type: application/x-www-form-urlencoded
+
+query=...
+
+POST /openrdf-sesame/repositories/test/statements HTTP/1.1
+Accept-Encoding: identity
+Content-Length: 227
+Host: localhost:8080
+Accept: */*
+User-Agent: sparqlwrapper 1.5.0 (http://sparql-wrapper.sourceforge.net/)
+Connection: close
+Content-Type: application/x-www-form-urlencoded
+
+update=...
+
+"""
